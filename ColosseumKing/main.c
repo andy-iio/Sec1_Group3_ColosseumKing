4fc6e38b1fe129678e490ba7e209c2c08c2db2f7
andy-iio <andyguest12@gmail.com>
2024-06-08 23:31:30 -0400

    inital file setup

---
+++ ColosseumKing/main.c
@@ -0,0 +1,16 @@
+//ColosseumKing
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//all our modules
+//#include "menu.h"
+//#include "attack.h"
+//#include "training.h"
+//#include "character.h"
+
+int main(void) {
+
+
+
+	return 0;
+}
