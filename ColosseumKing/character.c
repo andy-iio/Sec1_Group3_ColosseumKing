1f5ab8b51b8b51807984fd91c91f6c043f888858
andy-iio <andyguest12@gmail.com>
2024-07-06 09:18:16 -0400

    Added character file

---
+++ ColosseumKing/character.c
@@ -0,0 +1,18 @@
+#include "character.h"
+
+void increaseCharacterLevel() {
+
+}
+void increaseCharacterStats(union stats stat) {
+
+}
+void getCharacterStats() {
+
+}
+void getCharacterLevel() {
+
+}
+void increaseCharacterArmour() {
+
+}
+
