1f3fbe9eacfd9be339294c035e0d8131b676cea8
Andy <32138242+andy-iio@users.noreply.github.com>
2024-07-10 16:46:06 -0400

    changed functions to work with training module->removed get/set to use attributes instead (#9)

--- ColosseumKing/character.h
+++ ColosseumKing/character.h
@@ -1,70 +1,64 @@
 #pragma once
 #include <stdio.h>
 // Andy 
+
+//the types of enemies avaliable
+//these are temporary types we can change them
+enum enemyType {
+	MONSTER,
+	SWORDSMAN,
+	ALIEN
+};
+
+
 struct Character {
 	int health;
 	int attackDamage;
-	//also in here will be the characters ascii image
+	int strength;
+	int speed;
+	int coordination;
+	//also in here will be the characters ascii image if we decide to do that
 };
 
 struct Player {
 	struct Character;
 	char userName[50];
-	int strength;
-	int speeed;
-	int coordination;
+	int level;
+	int gearLevel;
 };
 
 struct Enemy {
 	struct Character;
-	char type;
+	enum enemyType type;
 };
 
 //----INITILIZE NEW CHARACTER----//
-//initialize a new player
-void initializePlayer(struct Player* player, char* username, int health, int attackDamage, int strength, int speed, int coordination);
 
 //load a player from save file 
-void loadPlayer(struct Player* player);
+void loadPlayer(struct Player* player, char* username, int health, int attackDamage, int strength, int speed, int coordination, int gearLevel);
 
-//initalize a new enemy
-void initializeEnemy(struct Enemy* enemy, char type, int health, int attackDamage);
+//initialize a new player with default values 
+void initializePlayer(struct Player* player, char* username);
+
+//initalize a new enemy with default values
+void initializeEnemy(struct Enemy* enemy, enum enemyType type);
+
+//load enemy from save file
+void loadEnemy(struct Enemy* enemy, enum enemyType type, int health, int attackDamage, int strength, int speed, int coordination);
 
 //----HEALTH----//
-//returns the current health of a character
-int getHealth(struct Character* character);
-
-//increase the health value of a character by x amount
-void increaseHealth(struct Character* character, int healthIncrease);
-
-//decrease the health value of a character by x amount
-void decreaseHealth(struct Character* character, int healthDecrease);
 
 //randomly increase the health of a character
 void randomHealthIncrease(struct Character* character);
 
-int getStrength(struct Character* character);
-int getSpeed(struct Character* character);
-int getCoordination(struct Character* character);
-int getGearLevel(struct Character* character);
+//----SKILLS----//
 
-void increaseStrength(struct Character* character, int increase);
-void increaseSpeed(struct Character* character, int increase);
-void increaseCoordination(struct Character* character, int increase);
-void increaseGearLevel(struct Character* character, int increase);
+//randomly increase a skill 
+void randomSkillIncrease(struct Character* character, char skill);
 
-//----ATTACK DAMAGE----//
-//returns the attack damage of a character
-int getAttackDamage(struct Character* character);
-
-//increase the characters attack damage by x amount
-void increaseAttackDamage(struct Character* character, int attackDamageIncrease);
-
-union stats {
-	char STAMINA;
-	char HEALTH;
-};
+//increase the level of a player by 1
+void increasePlayerLevel(struct Player* player);
 
 //----PRINT CHARACTER IMAGES----//
-//prints the ASCII image of a character
+//prints the ASCII image of a character through a txt file
 void printCharacterImage();
